b7bb9e197456fa52516acda1de18c6a1daf2d08f
Dmitrii <sushkovd98@mail.ru>
2021-03-12 11:25:54 +0100

    final code

--- src/App.vue
+++ src/App.vue
@@ -14,117 +14,36 @@
           <div class="row">
 
             <!-- Ролл -->
-            <div class="col-md-6">
-              <div class="card mb-4">
-                <img class="product-img" src="./img/roll/philadelphia.jpg" alt="">
-                <div class="card-body text-center">
-                  <h4 class="item-title">Филадельфия хит ролл</h4>
-                  <p><small class="text-muted">6 шт.</small></p>
-
-                  <div class="details-wrapper">
-                    <div class="items">
-                      <div class="items__control">-</div>
-                      <div class="items__current">1</div>
-                      <div class="items__control">+</div>
+              <div v-for="roll in rolls" :key="roll.name" class="col-md-6">
+                <div class="card mb-4">
+                  <img src="./img/roll/philadelphia.jpg" class="product-img" alt="roll">
+                  <div class="card-body text-center">
+                    <h4 class="item-title">{{roll.name}}</h4>
+                    <p><small class="text-muted">6 шт.</small></p>
+  
+                    <div class="details-wrapper">
+                      <div class="items">
+                        <div @click="roll.current > 1 ? roll.current-- : ''" class="items__control">-</div>
+                        <div class="items__current">{{roll.current}}</div>
+                        <div @click="roll.current++" class="items__control">+</div>
+                      </div>
+  
+                      <div class="price">
+                        <div class="price__weight">{{roll.grams}}</div>
+                        <div class="price__currency">{{roll.price}} ₽</div>
+                      </div>
                     </div>
 
-                    <div class="price">
-                      <div class="price__weight">180г.</div>
-                      <div class="price__currency">300 ₽</div>
-                    </div>
+                    <button
+                      @click="addToCart(roll.name, roll.price, roll.grams, roll.current), roll.current = 1"
+                      type="button" 
+                      class="btn btn-block btn-outline-warning">
+                     + в корзину
+                    </button>
+                    
                   </div>
-
-                  <button type="button" class="btn btn-block btn-outline-warning">+ в корзину</button>
-                  
                 </div>
               </div>
-            </div>
-            <!-- // Ролл -->
-
-            <!-- Ролл -->
-            <div class="col-md-6">
-              <div class="card mb-4">
-                <img class="product-img" src="./img/roll/california-tempura.jpg" alt="">
-                <div class="card-body text-center">
-                  <h4 class="item-title">Калифорния темпура</h4>
-                  <p><small class="text-muted">6 шт.</small></p>
-
-                  <div class="details-wrapper">
-                    <div class="items">
-                      <div class="items__control">-</div>
-                      <div class="items__current">1</div>
-                      <div class="items__control">+</div>
-                    </div>
-
-                    <div class="price">
-                      <div class="price__weight">205г.</div>
-                      <div class="price__currency">250 ₽</div>
-                    </div>
-                  </div>
-
-                  <button type="button" class="btn btn-block btn-outline-warning">+ в корзину</button>
-                  
-                </div>
-              </div>
-            </div>
-            <!-- // Ролл -->
-
-            <!-- Ролл -->
-            <div class="col-md-6">
-              <div class="card mb-4">
-                <img class="product-img" src="./img/roll/zapech-california.jpg" alt="">
-                <div class="card-body text-center">
-                  <h4 class="item-title">Запеченый ролл «Калифорния»</h4>
-                  <p><small class="text-muted">6 шт.</small></p>
-
-                  <div class="details-wrapper">
-                    <div class="items">
-                      <div class="items__control">-</div>
-                      <div class="items__current">1</div>
-                      <div class="items__control">+</div>
-                    </div>
-
-                    <div class="price">
-                      <div class="price__weight">182г.</div>
-                      <div class="price__currency">230 ₽</div>
-                    </div>
-
-                  </div>
-
-                  <button type="button" class="btn btn-block btn-outline-warning">+ в корзину</button>
-                  
-                </div>
-              </div>
-            </div>
-            <!-- // Ролл -->
-
-            <!-- Ролл -->
-            <div class="col-md-6">
-              <div class="card mb-4">
-                <img class="product-img" src="./img/roll/philadelphia.jpg" alt="">
-                <div class="card-body text-center">
-                  <h4 class="item-title">Филадельфия</h4>
-                  <p><small class="text-muted">6 шт.</small></p>
-
-                  <div class="details-wrapper">
-                    <div class="items">
-                      <div class="items__control">-</div>
-                      <div class="items__current">1</div>
-                      <div class="items__control">+</div>
-                    </div>
-
-                    <div class="price">
-                      <div class="price__weight">230г.</div>
-                      <div class="price__currency">320 ₽</div>
-                    </div>
-
-                  </div>
-
-                  <button type="button" class="btn btn-block btn-outline-warning">+ в корзину</button>
-                  
-                </div>
-              </div>
-            </div>
             <!-- // Ролл -->
 
           </div>
@@ -132,7 +51,7 @@
         <div class="col-md-4">
 
           <!-- Корзина Пустая -->
-          <div class="card mb-4">
+          <div v-if="orders.length == 0" class="card mb-4">
             <div class="card-body">
               <h5 class="card-title">Ваш заказ</h5>
               <div class="alert alert-secondary" role="alert">
@@ -143,7 +62,7 @@
           <!-- // Корзина Пустая -->
 
           <!-- Корзина Полная -->
-          <div class="card">
+          <div v-else class="card">
             <div class="card-body">
               <h5 class="card-title">Ваш заказ</h5>
               
@@ -151,88 +70,26 @@
               <div class="cart-wrapper">
 
                 <!-- cart-item  -->
-                <div class="cart-item">
+                <div v-for="(order, index) in orders" :key="order.name" class="cart-item">
                   <div class="cart-item__top">
                     <div class="cart-item__img">
                       <img src="./img/roll/philadelphia.jpg" alt="">
                     </div>
                     <div class="cart-item__desc">
-                      <div class="cart-item__title">Филадельфия хит ролл</div>
-                      <div class="cart-item__weight">6 шт. / 182г.</div>
+                      <div class="cart-item__title">{{order.name}}</div>
+                      <div class="cart-item__weight">6 шт. / {{order.grams}}</div>
 
                       <!-- cart-item__details -->
                       <div class="cart-item__details">
 
                         <div class="items items--small">
-                          <div class="items__control">-</div>
-                          <div class="items__current">1</div>
-                          <div class="items__control">+</div>
+                          <div @click="order.current > 1 ? order.current-- : deletingACard(index)" class="items__control">-</div>
+                          <div class="items__current">{{order.current}}</div>
+                          <div @click="order.current++" class="items__control">+</div>
                         </div>
 
                         <div class="price">
-                          <div class="price__currency">230 ₽</div>
-                        </div>
-
-                      </div>
-                      <!-- // cart-item__details -->
-
-                    </div>
-                  </div>
-                </div>
-                <!-- // cart-item  -->
-
-                <!-- cart-item  -->
-                <div class="cart-item">
-                  <div class="cart-item__top">
-                    <div class="cart-item__img">
-                      <img src="./img/roll/philadelphia.jpg" alt="">
-                    </div>
-                    <div class="cart-item__desc">
-                      <div class="cart-item__title">Филадельфия хит ролл</div>
-                      <div class="cart-item__weight">6 шт. / 182г.</div>
-
-                      <!-- cart-item__details -->
-                      <div class="cart-item__details">
-
-                        <div class="items items--small">
-                          <div class="items__control">-</div>
-                          <div class="items__current">1</div>
-                          <div class="items__control">+</div>
-                        </div>
-
-                        <div class="price">
-                          <div class="price__currency">230 ₽</div>
-                        </div>
-
-                      </div>
-                      <!-- // cart-item__details -->
-
-                    </div>
-                  </div>
-                </div>
-                <!-- // cart-item  -->
-
-                <!-- cart-item  -->
-                <div class="cart-item">
-                  <div class="cart-item__top">
-                    <div class="cart-item__img">
-                      <img src="./img/roll/philadelphia.jpg" alt="">
-                    </div>
-                    <div class="cart-item__desc">
-                      <div class="cart-item__title">Филадельфия хит ролл</div>
-                      <div class="cart-item__weight">6 шт. / 182г.</div>
-
-                      <!-- cart-item__details -->
-                      <div class="cart-item__details">
-
-                        <div class="items items--small">
-                          <div class="items__control">-</div>
-                          <div class="items__current">1</div>
-                          <div class="items__control">+</div>
-                        </div>
-
-                        <div class="price">
-                          <div class="price__currency">230 ₽</div>
+                          <div class="price__currency">{{order.price}} ₽</div>
                         </div>
 
                       </div>
@@ -244,10 +101,9 @@
                 <!-- // cart-item  -->
 
                 <div class="cart-total">
-                  <p><span class="h5">Доставка:</span> <span class="delivery-cost free">бесплатно</span> </p>
-                  <p><span class="h5">Итого:</span> <span class="total-price">330</span> <span class="rouble">₽</span></p>
+                  <p><span class="h5">Доставка:</span> <span class="delivery-cost free">{{freeShipping}}</span> </p>
+                  <p><span class="h5">Итого:</span> <span class="total-price">{{costCalculation}}</span> <span class="rouble">₽</span></p>
                 </div>
-
 
               </div>
               <!-- // cart-wrapper -->
@@ -282,9 +138,49 @@ export default {
 
   data() {
     return {
-      
+      rolls: [
+        {name: 'Филадельфия хит ролл', price: '300', grams: '180г.', current: 1},
+        {name: 'Калифорния темпура', price: '250', grams: '205г.', current: 1},
+        {name: 'Запеченый ролл «Калифорния»', price: '183', grams: '180г.', current: 1},
+        {name: 'Филадельфия', price: '300', grams: '420г.', current: 1},
+      ],
+      orders: [],
     }
-  }
+  }, 
+
+  computed: {
+    costCalculation() {
+      return this.orders.reduce((a, b) => a + (b.price * b.current), 0)
+    },
+
+    freeShipping() {
+      return this.costCalculation < 1000 ? '300 ₽' : 'Бесплатная'
+    }
+  },
+
+  methods: {
+    addToCart(name, price, grams, current) {
+      const order = { name, price, grams, current }
+      let similarityСheck = 0
+
+      this.orders.forEach(el => {
+        if (el.name == order.name) {
+          el.current = el.current + order.current
+          similarityСheck++
+          return
+        }
+      })
+
+      if (similarityСheck == 0) {
+        this.orders.push(order)
+        similarityСheck = 0
+      }
+    },
+
+    deletingACard(index) {
+      this.orders = this.orders.filter((el, ind) => index !== ind)
+    },
+  },
 }
 </script>
 
